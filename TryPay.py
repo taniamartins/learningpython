5fe60f34c1423ce13b96a1e086acb2a9fb070a54
Tania Martins <658084@soas.ac.uk>
2019-11-12 21:37:09 +0000

    TryPay
    The Pay.py and CondPay.py programmes, but with ability to throw an error input message through the try/except mechanism

---
+++ TryPay.py
@@ -0,0 +1,19 @@
+#chapter 3 - https://bit.ly/33TC1rS
+
+xh = input ("Enter Hours:")
+xr = input ("Enter Rate:")
+
+try:
+    fh = float(xh)
+    fr = float(xr)
+
+    if fh > 40 :
+        reg = fh * fr
+        otp = (fh - 40.0) * (fr * 0.5)
+        xp = reg + otp
+    else :
+        xp = fh * fr
+    print ("Your pay is", xp)
+
+except:
+    print ("Please input a number")
