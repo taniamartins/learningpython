b30e7e8bb4df93142e62eb79664e04353bdbab10
Tania Martins <658084@soas.ac.uk>
2019-11-20 17:19:03 +0000

    Square and Cube Root calculator
    Basic conditional programme that allows a choice between computing the square or cubed root of any input number

---
+++ rootcalc.py
@@ -0,0 +1,10 @@
+n = input("2/Square 3/Cube\n")
+xn = int(n)
+
+if xn == 2 :
+    sn = int(input("number: "))
+    print(sn * sn)
+
+elif xn == 3 :
+    cn = int(input("number: "))
+    print(cn * cn * cn)
